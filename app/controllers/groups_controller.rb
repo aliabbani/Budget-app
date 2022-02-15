4f85cfa2877c78bad20ba780529c9c4cd6a5239b
ali abbani <aliox@ahlam.com>
2022-02-15 19:23:25 +0200

    Add a new category page, The user fills out a form to create a new category, indicating their name and icon (both mandatory) & The user click (or taps) the Save button to create the new category, and is taken to the home page on success & When the user clicks on the Back button (<), the user navigates to the home page.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,32 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  def new; end
+  def new
+    @group = Group.new
+  end
 
-  def create; end
+  def create
+    @group = current_user.groups.new(group_params)
+    @group.user_id = current_user.id
+
+    respond_to do |format|
+      format.html do
+        if @group.save
+          flash[:success] = 'You have successfully created a category.'
+          redirect_to groups_url
+        else
+          flash.now[:error] = 'Error: Category could not be saved'
+          render :new
+        end
+      end
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
